bfc06789d0029cb1969c4d93c75ef1c8940d8d11
Jay <j.adeyina@gmail.com>
2023-03-05 21:49:31 +0900

    New pic solution

--- js/script.js
+++
@@ -1,5 +0,0 @@
-let heroImage = document.querySelector('.hero img');
-
-if(window.innerWidth >= "768px") {
-    heroImage.innerHTML = `<img src="../assets/image-hero-tablet.png" alt="hero-image" />`;
-}
